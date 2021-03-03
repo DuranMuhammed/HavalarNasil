d6edf11aac7dcd566a24e48e6940dee903110494
Muhammed Duran <77792756+DuranMuhammed@users.noreply.github.com>
2021-03-03 22:07:09 +0300

    Delete main directory

--- main/java/com/muhammedduran/havalarnasil/model/citiesLists/MainList.java
+++
@@ -1,9 +0,0 @@
-package com.muhammedduran.havalarnasil.model.citiesLists;
-
-import com.google.gson.annotations.SerializedName;
-
-public class MainList {
-
-    @SerializedName("temp")
-    public Float temp;
-}
